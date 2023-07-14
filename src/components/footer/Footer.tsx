9e943a1c8be71aefbca8db3ada87457e0f8d53d8
Neilly28 <neilpilarca@gmail.com>
2023-07-14 12:21:04 +0200

    add initial files

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./footer.scss";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>Neiladmin</span>
+      <span>Neildev admin dashboard</span>
+    </div>
+  );
+};
+
+export default Footer;
